40f6592daa2d4960f6f8fb1f65e7df6907f1ca05
vinizbjj <84604002+vinizbjj@users.noreply.github.com>
2022-06-15 14:29:57 -0300

    Update index.js

--- Exercicio 3/index.js
+++ Exercicio 3/index.js
@@ -9,6 +9,7 @@ var min = 999999999.9;
 var max = 0.0;
 var med = 0.0;
 var tamanho = 0.0;
+var ftdm = 0;
 
 for (const totalft of myJson) {
     if (totalft.valor > max) {
@@ -16,20 +17,21 @@ for (const totalft of myJson) {
     }
     if (totalft.valor < min && totalft.valor > 0) {
         min = totalft.valor
-    };
-
+    }
+    if (totalft.valor > 0) tamanho++
 
     console.log("dia", totalft.dia, "valor", totalft.valor)
 };
-
-
-for (const totalft of myJson) {
-    if (totalft.valor > 0) tamanho++
-
-};
 med = sum / tamanho;
 
+for (const totalft of myJson) {
+    if(totalft.valor > med)
+        ftdm ++
+    
+}
 
+
+console.log("Dias que o faturamento diario foram maio que a media: ",ftdm);
 console.log("Faturamento total foi de ", sum);
 console.log("Menor faturamento foi de:", min, "Maior faturamento foi de:", max);
 console.log("Media de faturamento é:", med);
